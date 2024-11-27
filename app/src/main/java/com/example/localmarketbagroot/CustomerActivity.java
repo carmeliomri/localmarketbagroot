e564c363063ced908fd23a6cee04317e2066a4ca
carmeli <carmeli.omri@gmail.com>
2024-11-27 15:53:52 +0200

    Added Seller activity, button sending to it works

--- app/src/main/java/com/example/localmarketbagroot/CustomerActivity.java
+++ app/src/main/java/com/example/localmarketbagroot/CustomerActivity.java
@@ -1,6 +1,10 @@
 package com.example.localmarketbagroot;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
 public class CustomerActivity extends AppCompatActivity {
 
+    String username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customer);
+        Intent intent = getIntent();
+        // Extract data from the intent
+        username = intent.getStringExtra("USERNAME");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myFunction(); // Call your function here
+            }
         });
     }
+
+
+
+    // Example function
+    private void myFunction() {
+        Intent intent = new Intent(CustomerActivity.this,SellerActivity.class);
+        intent.putExtra("USERNAME",username);
+        startActivity(intent);
+    }
+
 }
