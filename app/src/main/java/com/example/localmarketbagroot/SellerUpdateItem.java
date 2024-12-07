2ccec1b05c47dac4e7a8c0b1d2db7c830b402094
carmeli <carmeli.omri@gmail.com>
2024-12-07 11:58:21 +0200

    image appears on SellerUpdateItem

--- app/src/main/java/com/example/localmarketbagroot/SellerUpdateItem.java
+++ app/src/main/java/com/example/localmarketbagroot/SellerUpdateItem.java
@@ -1,6 +1,7 @@
 package com.example.localmarketbagroot;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
 
 public class SellerUpdateItem extends AppCompatActivity {
 
@@ -20,6 +23,19 @@ public class SellerUpdateItem extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             Toast.makeText(SellerUpdateItem.this, this.getIntent().getExtras().getInt("POSITION")+ "*******" +this.getIntent().getExtras().getString("URL"), Toast.LENGTH_SHORT).show();
+            // Reference to the ImageView
+            ImageView imageView = findViewById(R.id.imageView);
+
+            // URL of the image to load
+            String imageUrl = "https://via.placeholder.com/600";
+
+            // Use Glide to load the image into the ImageView
+            Glide.with(this)
+                    .load(this.getIntent().getExtras().getString("URL")) // The image URL
+                    //.placeholder(R.drawable.placeholder) // Optional placeholder while loading
+                    //.error(R.drawable.error_image) // Optional error image if loading fails
+                    .into(imageView);
+
             return insets;
         });
     }
